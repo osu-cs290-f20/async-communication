65b47928d84f019678275cc9c5b518a8b55de47b
Rob Hess <robwhess@gmail.com>
2020-12-01 16:02:12 -0800

    Add code from 12/1 lecture.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
@@ -28,6 +30,20 @@ app.get('/people/:person', function (req, res, next) {
     res.status(200).render('photoPage', peopleData[person]);
   } else {
     next();
+  }
+});
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  console.log("== req.body:", req.body);
+  if (req.body && req.body.url && req.body.caption) {
+    var person = req.params.person.toLowerCase();
+    if (peopleData[person]) {
+      // Add photo to DB...
+    } else {
+      next();
+    }
+  } else {
+    res.status(400).send("Request body must contain 'url' and 'caption'.")
   }
 });
 
